eb244435154552b9f3f35d3031b3873361eacd16
francovaro <francovaro@gmail.com>
2019-08-15 16:13:54 +0200

    Revert "Feed two"
    This reverts commit 22e20cefeae5cd210ea67c643b7a2dbce2582770.

--- src/dac.c
+++ src/dac.c
@@ -14,10 +14,8 @@
 static void DMA_DAC_Config(t_dac_channel channel);
 static void DMA_DAC_NVIC_Configuration(t_dac_channel channel);
 
-__IO uint16_t 	_dac_buffer_ch1[DAC_BUFFER_SIZE];
-__IO uint16_t 	_dac_buffer_ch2[DAC_BUFFER_SIZE];
-static uint8_t _dac_buffer_index_ch1;
-static uint8_t _dac_buffer_index_ch2;
+__IO uint16_t 	_dac_buffer[DAC_BUFFER_SIZE];
+static uint8_t _dac_buffer_index;
 
 /**
  * @brief Init DAC peripheral
@@ -33,6 +31,8 @@ uint8_t DAC_fv_init(t_dac_function function, t_dac_channel channel)
 	uint32_t trgo_source;
 
 	DAC_StructInit(&DAC_InitStructure);
+
+	// DAC_DeInit();
 
 	if (channel == e_dac_channel_1)
 	{
@@ -131,14 +131,12 @@ uint8_t DAC_fv_init(t_dac_function function, t_dac_channel channel)
 
 			if (channel == e_dac_channel_1)
 			{
-				_dac_buffer_index_ch1 = 0;
 				/* Set DAC channel1 DHR12RD register */
 				DAC_SetChannel1Data(DAC_Align_12b_R, 0x000);
 				TIM6_Config(100, 100);
 			}
 			else if (channel == e_dac_channel_2)
 			{
-				_dac_buffer_index_ch2 = 0;
 				/* Set DAC channel2 DHR12RD register */
 				DAC_SetChannel2Data(DAC_Align_12b_R, 0x000);
 				TIM7_Config(100, 100);
@@ -199,6 +197,9 @@ void DMA_DAC_Config(t_dac_channel channel)
 	
 	DMA_InitStructure.DMA_Channel = DMA_Channel_7;
 
+
+
+	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)&_dac_buffer;
 	DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
 	DMA_InitStructure.DMA_BufferSize = DAC_BUFFER_SIZE;
 	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
@@ -215,7 +216,6 @@ void DMA_DAC_Config(t_dac_channel channel)
 	if (channel == e_dac_channel_1)
 	{
 		DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)DAC->DHR12R1;
-		DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)&_dac_buffer_ch1;
 
 		DMA_Init(DMA1_Stream5, &DMA_InitStructure);
 		DMA_Cmd(DMA1_Stream5, ENABLE);
@@ -227,8 +227,6 @@ void DMA_DAC_Config(t_dac_channel channel)
 	else if (channel == e_dac_channel_2)
 	{
 		DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)DAC->DHR12R2;
-		DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)&_dac_buffer_ch2;
-
 		DMA_Init(DMA1_Stream6, &DMA_InitStructure);
 
 		DMA_Cmd(DMA1_Stream6, ENABLE);
@@ -262,17 +260,8 @@ void DMA_DAC_NVIC_Configuration(t_dac_channel channel)
 	NVIC_Init(&NVIC_InitStructure);
 }
 
-void DMA_Feed_Buffer(uint16_t newSample, t_dac_channel channel)
+void DMA_Feed_Buffer(uint16_t newSample)
 {
-	if (channel == e_dac_channel_1)
-	{
-		_dac_buffer_ch1[_dac_buffer_index_ch1] = newSample;
-		_dac_buffer_index_ch1 = (_dac_buffer_index_ch1 + 1) % DAC_BUFFER_SIZE;
-	}
-	else
-	{
-		_dac_buffer_ch2[_dac_buffer_index_ch2] = newSample;
-		_dac_buffer_index_ch2 = (_dac_buffer_index_ch2 + 1) % DAC_BUFFER_SIZE;
-	}
-
+	_dac_buffer[_dac_buffer_index] = newSample;
+	_dac_buffer_index = (_dac_buffer_index + 1) % DAC_BUFFER_SIZE;
 }
--- src/main.c
+++ src/main.c
@@ -38,10 +38,13 @@ int main(void)
 	TIM2_Config();
 	ADC_fv_Init();
 
-	DAC_FV_initPin();
+	TIM6_Config(90, 0);
+	//TIM7_Config();
 
-	DAC_fv_init(e_dac_buffer, e_dac_channel_2);
-	DAC_fv_init(e_dac_buffer, e_dac_channel_1);
+	//DAC_FV_initPin();
+
+	//DAC_fv_init(e_dac_triangle, e_dac_channel_2);
+	//DAC_fv_init(e_dac_triangle, e_dac_channel_1);
 
 	setSysTick (1000);
 	GPIO_WriteBit(GPIOG, GPIO_Pin_13, RESET);
@@ -52,16 +55,14 @@ int main(void)
 		if (SET == gDMA_FT_event)
 		{
 			gDMA_FT_event = RESET;
-			DMA_Feed_Buffer(ADC_return_val(1), e_dac_channel_1);
-			DMA_Feed_Buffer(ADC_return_val(1), e_dac_channel_2);
+			DMA_Feed_Buffer(ADC_return_val(1));
 			GPIO_ToggleBits(GPIOG, GPIO_Pin_13);
 		}
 
 		if (SET == gDMA_HT_event)
 		{
 			gDMA_HT_event = RESET;
-			DMA_Feed_Buffer(ADC_return_val(0), e_dac_channel_1);
-			DMA_Feed_Buffer(ADC_return_val(0), e_dac_channel_2);
+			DMA_Feed_Buffer(ADC_return_val(0));
 			GPIO_ToggleBits(GPIOG, GPIO_Pin_14);
 		}
 /*
