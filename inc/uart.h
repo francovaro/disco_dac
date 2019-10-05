00d646beb630ec558c8bbd5ff1eb10b4346d6cff
Francesco <francovaro@gmail.com>
2019-10-05 19:47:49 +0200

    Filez
    just added the usual files for uart....

---
+++ inc/uart.h
@@ -0,0 +1,13 @@
+/*
+ * uart.h
+ *
+ *  Created on: 05 ott 2019
+ *      Author: franc
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "stm32f4xx_usart.h"
+
+#endif /* UART_H_ */
---
+++ src/uart.c
@@ -0,0 +1,8 @@
+/*
+ * uart.c
+ *
+ *  Created on: 05 ott 2019
+ *      Author: franc
+ */
+
+#include "uart.h"
